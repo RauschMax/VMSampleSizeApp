dde78efdf7ebdbad5f0ebd33102b196d9bfa93f1
Rausch <Maximilian.Rausch@kantartns.com>
2017-12-07 13:13:53 +0100

    Layout nicer

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -16,37 +16,38 @@ dashboardPage(
 
   ####Body####
   body = dashboardBody(
-    fluidPage (
-      sidebarPanel(
-        radioButtons("SelIn", NULL, c("Enter Att/Lev" = "attlev", "Enter degrees of freedom directly" = "df"),
-                     selected = NULL, inline = TRUE, width = NULL),
-        conditionalPanel(condition = "input.SelIn == 'attlev'",
-                         numericInput("nAtt", label = "Enter Number of Attributes", value="2", min=1),
-                         uiOutput("variants")),
-        conditionalPanel(condition = "input.SelIn == 'df'",
-                         numericInput("DF", label = "Enter degrees of freedom (nLev - nAtt)", value="10", min=1)),
-        width = 3
-
-      ),
-      mainPanel(
-        div(style="display:inline-block", numericInput("nFixed", label = "Number of fixed alternatives", value="1", min=0)),
-        div(style="display:inline-block", numericInput("PercFixed", label = "% of fixed alternative expected", value="25", min=1)),
-        div(style="display:inline-block", numericInput("nConc", label = "Number of concepts per task (incl. Fixed)", value="4", min=1)),
-        div(style="display:inline-block", numericInput("HLI", label = "HLI (Homogeneity/Logical Consistency Index)", value="500", min=0, max = 500, step=50)),
-        hr(),
-        radioButtons("SelOutput", "Choose your output:", c("Number of Tasks" = "ntasks", "Sample size" = "nresp"),
-                     selected = NULL, inline = TRUE, width = NULL),
-        conditionalPanel(condition = "input.SelOutput == 'nresp'",
-                         numericInput("nTasks", label = "Number of tasks per respondent", value="12", min=0),
-                         tableOutput("table_nResp")),
-        conditionalPanel(condition = "input.SelOutput == 'ntasks'",
-                         numericInput("nResp", label = "Sample size", value="500", min=0),
-                         tableOutput("table_nTask")),
-        # updateNumericInput(session, "nResp", value = output$nResp_cal),
-        width = 5
-      )
+    tags$head(tags$style(HTML('.box {min-height: 385px;}'))),
+    fluidRow(
+      box(title = "Parameters", width = 3, status = "primary", solidHeader = TRUE,
+                 radioButtons("SelIn", NULL, c("ValueDriver" = "driver", "ValuePricer" = "pricer"),
+                              selected = NULL, inline = TRUE, width = NULL),
+                 conditionalPanel(condition = "input.SelIn == 'driver'",
+                                  numericInput("nAtt", label = "Enter Number of Attributes", value="5", min=1, width="425px"),
+                                  uiOutput("variants")),
+                 conditionalPanel(condition = "input.SelIn == 'pricer'",
+                                  numericInput("SKU", label = "Enter number of SKUs/products:", value="20", min=2),
+                                  numericInput("price", label = "Enter number of prices per SKU/product:", value="5", min=1))
+             ),
+      box(title = "Settings", width = 2, status = "success", solidHeader = TRUE,
+                 numericInput("nFixed", label = "Number of fixed alternatives", value="1", min=0),
+                 numericInput("PercFixed", label = "% of fixed alternative expected", value="25", min=1),
+                 numericInput("nConc", label = "Number of concepts per task (incl. Fixed)", value="4", min=1),
+                 numericInput("HLI", label = "HLI (Homogeneity/Logical Consistency Index)", value="500", min=0, max = 500, step=50)
+             ),
+      box(title = "Recommendation", width = 2, status = "danger", solidHeader = TRUE,
+                 radioButtons("SelOutput", "Choose your output:", c("Number of Tasks" = "ntasks", "Sample size" = "nresp"),
+                              selected = NULL, inline = TRUE, width = NULL),
+                 conditionalPanel(condition = "input.SelOutput == 'nresp'",
+                                  numericInput("nTasks", label = "Number of tasks per respondent", value="12", min=0),
+                                  tags$hr(),
+                                  infoBoxOutput("nRespBox")),
+                 conditionalPanel(condition = "input.SelOutput == 'ntasks'",
+                                  numericInput("nResp", label = "Sample size", value="500", min=0),
+                                  tags$hr(),
+                                  infoBoxOutput("nTaskBox"))
+             )
     )
-),
+  ),
 
 #Title change from default
 title = 'Sample Size Calculator App',
